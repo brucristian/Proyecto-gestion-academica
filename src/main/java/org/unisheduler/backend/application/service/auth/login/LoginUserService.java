3bd4c7d68d10a027e51779442bbd6b48cc9b1737
joan566 <joansebastianvergaravalencia@gmail.com>
2026-06-01 12:59:59 -0500

    Se implemento el caso de uso de inicio de sesion

--- src/main/java/org/unisheduler/backend/application/service/auth/login/LoginUserService.java
+++ src/main/java/org/unisheduler/backend/application/service/auth/login/LoginUserService.java
@@ -5,14 +5,17 @@ import org.unisheduler.backend.application.service.auth.login.dtos.UserInfo;
 import org.unisheduler.backend.domain.model.auth.entity.User;
 import org.unisheduler.backend.domain.port.in.auth.LoginUserUseCase;
 import org.unisheduler.backend.domain.port.out.auth.UserRepository;
+import org.unisheduler.backend.domain.port.out.security.PasswordEncoderPort;
 
 import java.util.Optional;
 
 public class LoginUserService implements LoginUserUseCase {
     private final UserRepository userRepository;
+    private final PasswordEncoderPort passwordEncoderPort;
 
-    public LoginUserService(UserRepository userRepository) {
+    public LoginUserService(UserRepository userRepository, PasswordEncoderPort passwordEncoderPort) {
         this.userRepository = userRepository;
+        this.passwordEncoderPort = passwordEncoderPort;
     }
 
     @Override
@@ -20,16 +23,31 @@ public class LoginUserService implements LoginUserUseCase {
         Optional<User> userOptional = userRepository.findByEmail(command.getEmail());
 
         if(userOptional.isEmpty()) {
+            System.out.println("Case 1");
             return new LoginUserResponse(
                     false,
-                    "Usuario no encontrado",
+                    "Usuario o contraseña incorrectos",
                     null
             );
         }
 
         User user = userOptional.get();
 
-        UserInfo userInfo = new UserInfo();
+        if(!passwordEncoderPort.matches(command.getPassword(), user.getPassword().getValue())) {
+            System.out.println("Case 2");
+            return new LoginUserResponse(
+                    false,
+                    "Usuario o contraseña incorrectos",
+                    null
+            );
+        }
+
+        UserInfo userInfo = new UserInfo(
+                user.getUserId(),
+                user.getFirstName() + " " + user.getLastName(),
+                user.getEmail().getValue(),
+                user.getRole().name()
+        );
 
         return new LoginUserResponse(
                 true,
--- src/main/java/org/unisheduler/backend/application/service/auth/login/dtos/UserInfo.java
+++ src/main/java/org/unisheduler/backend/application/service/auth/login/dtos/UserInfo.java
@@ -1,4 +1,31 @@
 package org.unisheduler.backend.application.service.auth.login.dtos;
 
 public class UserInfo {
+    private final String userId;
+    private final String fullName;
+    private final String email;
+    private final String role;
+
+    public UserInfo(String userId, String fullName, String email, String role) {
+        this.userId = userId;
+        this.fullName = fullName;
+        this.email = email;
+        this.role = role;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getRole() {
+        return role;
+    }
 }
